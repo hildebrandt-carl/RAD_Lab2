2c71b7aedee5bdbb0d2c806861b421e73c7e9cb6
hildebrandt-carl <hildebrandt.carl@outlook.com>
2017-09-26 18:48:18 -0500

    Added some of the main function code

--- Watchdog_Flasher/watchdog.c
+++ Watchdog_Flasher/watchdog.c
@@ -1,29 +1,49 @@
 #include "header.h"
 static uint8_t PROGRESS=0;
 /*counters to keep track of task progress*/
-/*static uint16_t count=0;
+static uint16_t count=0;
 static uint8_t blinkcounter1=0;
 static uint8_t blinkcounter2=0;
-static uint8_t blinkcounter3=0;*/
+static uint8_t blinkcounter3=0;
 
 static struct etimer et;
 
 PROCESS(main_process, "Main Task");
 AUTOSTART_PROCESSES(&main_process);
 
-/*void incrementCOUNTER(uint8_t i){
-
-	
-
+void incrementCOUNTER(uint8_t i)
+{
+	switch(i)   
+	{  
+		case 1:  
+			blinkcounter1++;  
+		case 2:  
+			blinkcounter2++;  
+		case 3:  
+			blinkcounter3++;  
+		default :  
+			printf("ERROR! Wrong counter incremement value");  
+	}
 }
 
-
-uint8_t getCOUNTER(uint8_t i){
-		return -1;	
+uint8_t getCOUNTER(uint8_t i)
+{
+	switch(i)   
+	{  
+		case 1:  
+			return(blinkcounter1);  
+		case 2:  
+			return(blinkcounter2);  
+		case 3: 
+			return(blinkcounter3);
+		default :  
+			printf("ERROR! Wrong counter get value");  
+	}	
 }
 
-uint8_t getPROGRESS(){
-return PROGRESS;
+uint8_t getPROGRESS()
+{
+	return PROGRESS;
 }
 
 void clearPROGRESS(){
@@ -33,7 +53,7 @@ void clearPROGRESS(){
 void setPROGRESS(uint8_t i){
 
 
-}*/
+}
 
 PROCESS_THREAD(main_process, ev, data){
 
@@ -44,7 +64,7 @@ PROCESS_THREAD(main_process, ev, data){
 	//WDTCTL = WDTPW + WDTCNTCL + WDTHOLD +WDTISx;
 
 	//start the watchdog
-	//WDTCTL = (WDTCTL_L&~(WDTHOLD))+ WDTPW;
+	WDTCTL = (WDTCTL_L&~(WDTHOLD))+ WDTPW;
 	
 	WDTCTL = WDTPW + WDTCNTCL + WDTHOLD + WDTIS0;
 	
@@ -56,10 +76,9 @@ PROCESS_THREAD(main_process, ev, data){
 
 	process_start(&LED1,NULL);
 	
-	//process_start(&LED2, NULL);
+	process_start(&LED2, NULL);
 	
-	//process_start(&LED3,NULL);
-
+	process_start(&LED3,NULL);	
  	
 	}
 	PROCESS_END();
